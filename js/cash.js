43a4ae25f4b16bf7800f8005788a4fb9be383343
Anikroy7 <roy645800@gmail.com>
2022-02-17 01:46:05 +0600

    bonus section added

--- js/cash.js
+++ js/cash.js
@@ -21,7 +21,7 @@ function getInputValue(inputId) {
     let inputField = document.getElementById(inputId);
     let inputNumberText = inputField.value;
     let inputNumber = parseFloat(inputNumberText);
-    if (isNaN(inputNumber)) {
+    if (isNaN(inputNumber) || inputNumber < 0) {
         alert('Please input a valid number');
     }
     else {
@@ -29,3 +29,19 @@ function getInputValue(inputId) {
     }
 
 }
+
+// --------------  bonous section start
+
+document.getElementById('save-button').addEventListener('click', function () {
+    let totalIncome = getInputValue('income-input');
+    let saveInputNumber = getInputValue('save-input');
+    let savingAmount = (totalIncome * saveInputNumber) / 100;
+    let savingAmountId = document.getElementById('saving-amount');
+    savingAmountId.innerText = savingAmount;
+    let totalBalanceAmountText = document.getElementById('total-balance').innerText;
+    let totalBalanceAmount = parseFloat(totalBalanceAmountText);
+    let remainingBalance = totalBalanceAmount - savingAmount;
+    let remainingBalanceId = document.getElementById('remaining-balance');
+    remainingBalanceId.innerText = remainingBalance;
+
+});
